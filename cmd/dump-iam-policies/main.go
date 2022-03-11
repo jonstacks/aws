c9fb08aee7dfb069649130120536849ec38ec518
Jonathan Stacks <jonstacks13@gmail.com>
2022-03-11 13:04:59 -0600

    Faster searching and only filter to dynamo policies

--- cmd/dump-iam-policies/main.go
+++ cmd/dump-iam-policies/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/jonstacks/aws/pkg/models"
 	"github.com/jonstacks/aws/pkg/utils"
 )
@@ -20,13 +22,23 @@ func main() {
 	fmt.Printf("Got %d policies\n", len(policies))
 
 	for _, policy := range policies {
-		fmt.Printf("=============================\n%s\n=============================\n", aws.StringValue(policy.Arn))
-		detail, err := models.DescribePolicy(*policy.Arn, *policy.DefaultVersionId)
+		var detail *iam.GetPolicyVersionOutput
+		for i := 0; i < 3; i++ {
+			detail, err = models.DescribePolicy(*policy.Arn, *policy.DefaultVersionId)
+			if err == nil {
+				break
+			}
+			time.Sleep(time.Second * 5)
+		}
 		utils.ExitErrorHandler(err)
 
 		doc := detail.PolicyVersion.Document
 		decodedValue, _ := url.QueryUnescape(*doc)
-		fmt.Printf("%s\n", decodedValue)
-		time.Sleep(2 * time.Second)
+		if strings.Contains(decodedValue, "dynamo") || strings.Contains(decodedValue, "table") {
+			fmt.Printf("=============================\n%s\n=============================\n", aws.StringValue(policy.Arn))
+			fmt.Printf("%s\n", decodedValue)
+		} else {
+			fmt.Printf("No dynamo policy found in %s\n", aws.StringValue(policy.Arn))
+		}
 	}
 }
